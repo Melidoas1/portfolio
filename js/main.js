ecec253b7c1447d9d8e52f0f199c851a660a220b
Melidoas1 <nikita.prokopenko1997@gmail.com>
2024-03-21 20:18:10 +0300

    addJS

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(window).on('load', function(){
 
     // vide.js - video bg
     $('#header').vide({
-        mp4: '../video/cover',}
+        mp4: './video/cover',}
         , {
         bgColor: '#3c413f'
     });
